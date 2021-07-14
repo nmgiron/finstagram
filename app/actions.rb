ac812628ff782a96fd80b10de65e34feae3ccffc
nmgiron <nmgiron@gmail.com>
2021-07-14 07:57:06 +0000

    W3D1 Embedding Ruby

--- app/actions.rb
+++ app/actions.rb
@@ -6,7 +6,7 @@ def humanized_time_ago(time_ago_in_minutes)
     end
 end
 get '/' do
- finstagram_post_shark = {
+ @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -19,7 +19,7 @@ get '/' do
       }]
  }
  
- finstagram_post_whale = {
+ @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -32,7 +32,7 @@ get '/' do
     }]
   }
 
- finstagram_post_marlin ={
+ @finstagram_post_marlin ={
     username: "marlin-peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -44,5 +44,7 @@ get '/' do
       text: "lunchtime! :)"
     }]
  }
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s   
+ @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+  
+  erb(:index)   
 end    
