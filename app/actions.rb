028942808aecdf70867446b282059c8ad2ce679d
nmgiron <nmgiron@gmail.com>
2021-07-29 02:36:03 +0000

    W5D2 Displaying Posts and Cleaning up

--- app/actions.rb
+++ app/actions.rb
@@ -54,3 +54,25 @@ helpers do
     User.find_by(id: session[:user_id])
   end
 end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show") 
+end
